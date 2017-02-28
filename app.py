a7cb24f946597d94f979f953e5515923091aa6bd
qqjin <445741994@qq.com>
2017-02-28 16:09:40 +0800

    pys

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-import logging
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+	return web.Response(body=b'<h1>anwsome</h1>')
+
+@asyncio.coroutine
+def init(loop):
+	app = 
